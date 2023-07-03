b382fd6017ddd5914bf44f6645f7a1c92eb7c3c1
Paola <paola.patino.castillo@gmail.com>
2023-07-03 20:13:33 +0200

    FEAT: Create and write results in a json file

--- ReadMe.md
+++ ReadMe.md
@@ -1,9 +1,15 @@
-# Scraper!
+# Scraper
 
 Scraper is a Python script for basic web scraping.
 
-1. Install the dependencies:
-- Beautiful soup `pip install beautifulsoup4`
+0. You will need to have Python3 installed in your computer in order to run this program. 
 
-**Note:** You can do this globally to run the app, but you as an alternative you can create a virtual enviroment for the project using venv. The second approach is usually recommended as it won't interfere or cause dependency conficts with any other projects you run in your machine.
+1. Install dependencies: Beautiful soup.
+- Install packages automatically running `pip install -r requirements.txt` in your command line.
+- Or, install manually the 4.12.2 version of Beautiful soup `pip install beautifulsoup4==4.12.2`
 
+**Note:** You can do this globally to run the app or, as an alternative, you can create a virtual enviroment for the project using `venv`. The second approach is usually recommended as it won't interfere or cause dependency conflicts with any other projects you run in your machine.
+
+2. From the root folder, run the scraper.py on your CLI: `python3 scraper.py`. This will create a json file containing the data retrieved.
+
+3. You can run `cat contact_results.json` to visualize this file.
--- scraper.py
+++ scraper.py
@@ -1,14 +1,17 @@
 import urllib.request, urllib.parse, urllib.error
 import re
+import json
 from bs4 import BeautifulSoup
 
-url = input('Enter a URL: ')
-html = urllib.request.urlopen(url)
+contact_url = 'https://reviewpro.shijigroup.com/team#contact'
+html = urllib.request.urlopen(contact_url)
 soup = BeautifulSoup(html, 'html.parser')
 
 contact_grid_sections = soup.find('div', class_= 'contact_grid').find_all('h3')
 customer_suport_string_refs = ['EMEA', 'APAC', 'China', 'USA', 'Australia & New Zealand']
-mail_re = '^mailto:(.+)'
+# mail_re = '^mailto:(.+\.com)' # To keep the query url use this regular expression instead: '^mailto:(.+)' 
+mail_re = r'[\w\.-]+@[\w\.-]+'
+contact_results = {}
 
 def getNextAnchor(custom_re, tag=None, text=None, section=None):
     if section == None : 
@@ -17,11 +20,19 @@ def getNextAnchor(custom_re, tag=None, text=None, section=None):
     return re.findall(custom_re, rawLink)[0]
 
 for contact_section in contact_grid_sections:
-  print(contact_section.contents[0])
   if contact_section.contents[0] == 'Customer Support':
-     for string_ref in customer_suport_string_refs:
-      print('·',string_ref, ':', getNextAnchor(mail_re, 'div', string_ref, ))
+    references = {}
+    for string_ref in customer_suport_string_refs:
+      references[string_ref] = getNextAnchor(mail_re, 'div', string_ref, )
+    contact_results['Customer Support'] = references
   else:
-     print('·', getNextAnchor(mail_re, None, None, section=contact_section))
-
-#  https://reviewpro.shijigroup.com/team#contact
+    if len(contact_section.fetchNextSiblings('a')) > 1:
+        links = []
+        for link in contact_section.fetchNextSiblings('a'):
+           links.append(re.findall(mail_re, link.get('href'))[0])
+        contact_results[contact_section.contents[0]] = links
+    else:
+       contact_results[contact_section.contents[0]] = getNextAnchor(mail_re, None, None, section=contact_section)
+       
+with open('contact_results.json', 'w') as json_file:
+    json.dump(contact_results, json_file, indent=4)
